0fa4b135766d85cce199b71a1cb9b39e184ccb88
Reem <remalirema931@gmail.com>
2024-03-18 14:02:36 -0700

    add

--- src/home.jsx
+++ src/home.jsx
@@ -4,11 +4,8 @@ import './App.css'
 import Design from './design'
 import { useDispatch ,useSelector} from 'react-redux'
 import { logout } from './userReducer'
-import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import { useGetPostsQuery } from './features/posts/postSlice'
 export default function Home(){
-    const {isLoading}= useGetPostsQuery()
     const navigate=useNavigate()
     const user=useSelector(state=>state.auth.user)
     const dispatch=useDispatch()
@@ -22,13 +19,13 @@ export default function Home(){
         <div >
         <img src='./cover.jpg' className='w-full relative h-full blur-sm'/>
         <Design />
-       <h2 className='absolute top-20 left-[40%]  text-2xl font-bold text-slate-800 cursor-auto underline hover:scale-110'>{!isLoading ? `Our Subjects`  : <Skeleton width={150} baseColor='gray' />}</h2>
+       <h2 className='absolute top-20 left-[40%]  text-2xl font-bold text-slate-800 cursor-auto underline hover:scale-110'>Our Subjects</h2>
         <div className='flex flex-row flex-wrap gap-5 absolute w-[600px] h-64 top-[50%]  left-[50%]  -translate-x-[50%] -translate-y-[50%] z-2 ' >
         {subjects.map(subject=>
-           !isLoading ? <div key={subject.id} className='relative w-36 h-24 m-2 home hover:scale-105 '  onClick={()=>navigate(`postList/${subject.title}`)}> 
+          <div key={subject.id} className='relative w-36 h-24 m-2 home hover:scale-105 '  onClick={()=>navigate(`postList/${subject.title}`)}> 
            <img src={subject.src} alt={subject.title} className='relative w-full h-full rounded-full  brightness-50'  />
            <div className='absolute inset-0 flex  items-center justify-center'><h3 className='font-bold  text-xl  text-slate-100 font-serief cursor-pointer'>{subject.title}</h3></div>
-           </div> : <Skeleton baseColor='gray' className='w-36 h-28 rounded-full' />
+           </div>
            
     )}
         
@@ -38,3 +35,4 @@ export default function Home(){
         </div>
     )
 }
+// : <Skeleton baseColor='gray' className='w-36 h-28 rounded-full' />
