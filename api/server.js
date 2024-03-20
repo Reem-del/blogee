16efb59ad35c3cbf589fca3642fd9eaaf5e56572
Reem <remalirema931@gmail.com>
2024-03-20 15:30:50 -0700

    json-server

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const jsonServer=require('json-server');
 const path=require('path');
+try{
 const server=jsonServer.create();
 const router=jsonServer.router(path.join(__dirname,'Data','db.json'));
 const middlewares=jsonServer.defaults()
@@ -7,6 +8,8 @@ server.use(middlewares)
 server.use(router)
 const Port=3001;
 server.listen('port',()=>{
-    console.log(`json server running on port ${Port}`)
-})
+    console.log(`json server running on port ${Port}`)})
+}
+catch(error){(console.error())}
+
 module.exports=server
